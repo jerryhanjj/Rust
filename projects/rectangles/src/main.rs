fab52020afcfa7e44ede043cc5534e94e36b675b
Capsule <jerryhanjj@126.com>
2024-12-18 21:51:53 +0800

    rectangles v0.2, 使用结构体重构

--- projects/rectangles/src/main.rs
+++ projects/rectangles/src/main.rs
@@ -1,3 +1,8 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
 fn main() {
     let width1 = 30;
     let height1 = 50;
@@ -6,6 +11,21 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area(width1, height1)
     );
+
+    // 使用结构体重构
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area_1(&rect1)
+    );
+}
+
+// 计算面积
+fn area_1(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
 }
 
 fn area(width: u32, height: u32) -> u32 {
