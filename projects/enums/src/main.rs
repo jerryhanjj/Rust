ab66a9c659cab8608ea68f3583f2c273a379ee86
Capsule <jerryhanjj@126.com>
2024-12-19 21:42:01 +0800

    定义并使用一个enum

--- projects/enums/src/main.rs
+++ projects/enums/src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+fn route(ip_type: IpAddrKind) {
+    println!("ip_type: {:?}", ip_type);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+    let six = IpAddrKind::V6;
+    println!("four: {:?}, six: {:?}", four, six);
+
+    route(IpAddrKind::V4);
+    route(IpAddrKind::V6);
 }
