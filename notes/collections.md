6d9bab62c6cf978670b3a0e95e1c8d1280ba429f
Capsule <jerryhanjj@126.com>
2024-12-23 23:35:33 +0800

    通过索引或者get方法获得vector的项

---
+++ notes/collections.md
@@ -0,0 +1,12 @@
+## 集合
+Rust 标准库中提供了一系列集合：
+- Sequences: Vec, VecDeque, LinkedList
+- Maps: HashMap, BTreeMap
+- Sets: HashSet, BTreeSet
+- Misc: BinaryHeap
+
+## Vector
+
+- `vector` 允许我们在一个单独的数据结构中储存多于一个的值，它在内存中彼此相邻地排列所有的值。
+- `vector` 只能储存相同类型的值
+- 通过 **索引** 或者 **get** 方法获取 `vector` 中的项，如果索引越界，前者会导致程序 `panic` ，后者会返回一个 `None`
---
+++ projects/collections/src/main.rs
@@ -0,0 +1,30 @@
+fn main() {
+    let v = vec![1, 2, 3];
+
+    // vector 可以通过放入其中的值来推断类型
+    let mut v1 = Vec::new();
+    v1.push(5);
+    v1.push(6);
+
+    // 通过索引获取 vector 中的值
+    let third = &v[2];
+    println!("The third element is {third}");
+
+    // 通过 get 方法获取 vector 中的项
+    let third = v.get(3);
+    match third {
+        Some(third) => println!("The third element is {third}"),
+        None => println!("There is no third element."),
+    }
+
+    // 我们在 third 处通过get方法获取了 v 的不可变引用，下方 push 时出错
+    // 再一次验证 作用域内  不能同时存在可变和不可变引用
+    // v.push(4);
+
+    /*
+    为什么第一个元素的引用会关心 vector 结尾的变化？
+    不能这么做的原因是由于 vector 的工作方式：在 vector 的结尾增加新元素时，
+    在没有足够空间将所有元素依次相邻存放的情况下，可能会要求分配新内存并将老的元素拷贝到新的空间中。
+    这时，第一个元素的引用就指向了被释放的内存。借用规则阻止程序陷入这种状况。
+    */
+}
