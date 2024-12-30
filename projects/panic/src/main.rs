aaff153affe74080ad2214d84ba76356dca8bb6c
Capsule <jerryhanjj@126.com>
2024-12-31 00:10:46 +0800

    panic处理中的传播错误

--- projects/panic/src/main.rs
+++ projects/panic/src/main.rs
@@ -1,4 +1,6 @@
-use std::{fs::File, io::ErrorKind};
+use std::f32::consts::E;
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
 
 fn main() {
     // panic!("crash and burn");
@@ -28,5 +30,21 @@ fn main() {
     // 使用 expect 定制错误信息
     let greeting_file = File::open("hello.txt").expect("error msg");
 
-    
+    // 传播错误
+    // 选择让调用者知道这个错误并决定该如何处理
+    fn read_username_from_file() -> Result<String, io::Error> {
+        let username_file_result = File::open("hello.txt");
+
+        let mut username_file = match username_file_result {
+            Ok(file) => file,
+            Err(e) => return Err(e),
+        };
+
+        let mut username = String::new();
+
+        match username_file.read_to_string(&mut username) {
+            Ok(_) => Ok(username),
+            Err(e) => Err(e),
+        }
+    }
 }
