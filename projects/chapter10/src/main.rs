a8404ee5f87d92fe36b41d65f18f18e79cc6b626
Capsule <jerryhanjj@126.com>
2025-07-07 01:31:21 +0800

    .gitignore add .idea dir and change chapter10 related files (long time ago)

--- projects/chapter10/src/main.rs
+++ projects/chapter10/src/main.rs
@@ -10,6 +10,18 @@ fn largest(list: &[i32]) -> &i32 {
     largest
 }
 
+fn largest_t<T>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
     let number_list = vec![34, 50, 25, 100, 65];
     let result = largest(&number_list);
